3ebbb07df521349c6a7805fa7f1737220297a1f5
seibow <airwhite2525@gmail.com>
2025-09-06 11:51:28 +0900

    second commit

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return 'Hello World'
 
+@app.route('/test')
+def index_test():
+    return 'Hello TEST!!'
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
